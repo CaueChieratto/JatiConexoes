c896d58ccf55e0326b93baeb708d674bae6bebe3
CaueChieratto <caue.chieratto@icloud.com>
2024-10-11 11:47:55 -0300

    error page

--- src/Jati.js
+++ src/Jati.js
@@ -2,9 +2,12 @@ import React from "react";
 import Pagina_inicial from "./paginas/Pagina_inicial";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Produtos from "./paginas/Produtos";
+import Error from "./paginas/Error.js";
+
 const router = createBrowserRouter([
   { path: "/", element: <Pagina_inicial></Pagina_inicial> },
   { path: "/produtos", element: <Produtos></Produtos> },
+  { path: "*", element: <Error></Error> },
 ]);
 
 export default function Jati() {
---
+++ src/paginas/Error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <>
+      <h1>Página não encontrada</h1>
+      <p>
+        Oops! A página que você está procurando não existe, tente essa
+        <a href="https://jaticonexoes.com.br/">Jati Conexões</a>
+      </p>
+    </>
+  );
+}
